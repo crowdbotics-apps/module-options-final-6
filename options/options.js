1acfc50715eab833be2e769a79e033bafed7bb1f
Crowdbotics <team@crowdbotics.com>
2021-09-16 13:56:35 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,6 +1,15 @@
+const newStyles = StyleSheet.create({
+  hr: { marginTop: 20, borderBottomColor: "black" }
+})
 export const globalOptions = {
   name: "module_options_final_6Identifier",
   url: "https://module_options_final_6Identifier.botics.co",
   api: "https://module_options_final_6Identifier.botics.co/api/v1"
 }
-export const modulesOptions = {}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    title: "App Menu #2",
+    copy: "Routes available #1",
+    styles: newStyles
+  }
+}
